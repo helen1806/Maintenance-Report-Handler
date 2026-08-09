126df9e5a88965aed19e9e7e08181525c8141b7e
helen <helenmarys1023@gmail.com>
2026-08-09 12:34:07 +0530

    modified background worker

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start the Celery worker in the background (notice the & at the end)
-celery -A app.celery_app worker --loglevel=info &
+# Start the Celery worker in the background WITH ONLY 1 CONCURRENT PROCESS
+celery -A app.celery_app worker --loglevel=info --concurrency=1 &
 
 # Start the FastAPI web server in the foreground
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
